cf330c75117829145ce88b294fc7ce9e83f98b16
musabalki <musabalkimb@gmail.com>
2021-05-24 23:59:03 +0300

    Edit blog page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -11,6 +11,86 @@ const Blog = () => {
                     </div>
                 </div>
             </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3">
+                        <div className="blog-item">
+                            <div className="blog-img">
+                                <img src="/img/product1.jpeg" className="w-100" />
+                            </div>
+                            <div className="blog-content">
+                                <h2 className="blog-title">
+                                    Capitalize on low hanging fruit to identify a ballpark test
+                                </h2>
+                                <div className="blog-info">
+                                    <div className="blog-info-item"><i class="far fa-calendar-alt "></i><span>20.10.2020</span></div>
+                                    <div className="blog-info-item"><i class="far fa-comments"></i><span>0 Comments</span></div>
+                                </div>
+                                <div className="blog-text">
+                                    Lorem ipsum dolor sit amet, consectetur adipisi cing elit.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="blog-item">
+                            <div className="blog-img">
+                                <img src="/img/product1.jpeg" className="w-100" />
+                            </div>
+                            <div className="blog-content">
+                                <h2 className="blog-title">
+                                    Capitalize on low hanging fruit to identify a ballpark test
+                                </h2>
+                                <div className="blog-info">
+                                    <div className="blog-info-item"><i class="far fa-calendar-alt "></i><span>20.10.2020</span></div>
+                                    <div className="blog-info-item"><i class="far fa-comments"></i><span>0 Comments</span></div>
+                                </div>
+                                <div className="blog-text">
+                                    Lorem ipsum dolor sit amet, consectetur adipisi cing elit.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="blog-item">
+                            <div className="blog-img">
+                                <img src="/img/product1.jpeg" className="w-100" />
+                            </div>
+                            <div className="blog-content">
+                                <h2 className="blog-title">
+                                    Capitalize on low hanging fruit to identify a ballpark test
+                                </h2>
+                                <div className="blog-info">
+                                    <div className="blog-info-item"><i class="far fa-calendar-alt "></i><span>20.10.2020</span></div>
+                                    <div className="blog-info-item"><i class="far fa-comments"></i><span>0 Comments</span></div>
+                                </div>
+                                <div className="blog-text">
+                                    Lorem ipsum dolor sit amet, consectetur adipisi cing elit.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="blog-item">
+                            <div className="blog-img">
+                                <img src="/img/product1.jpeg" className="w-100" />
+                            </div>
+                            <div className="blog-content">
+                                <h2 className="blog-title">
+                                    Capitalize on low hanging fruit to identify a ballpark test
+                                </h2>
+                                <div className="blog-info">
+                                    <div className="blog-info-item"><i class="far fa-calendar-alt "></i><span>20.10.2020</span></div>
+                                    <div className="blog-info-item"><i class="far fa-comments"></i><span>0 Comments</span></div>
+                                </div>
+                                <div className="blog-text">
+                                    Lorem ipsum dolor sit amet, consectetur adipisi cing elit.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
